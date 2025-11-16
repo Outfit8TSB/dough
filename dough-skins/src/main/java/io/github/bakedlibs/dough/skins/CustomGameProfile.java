85dae2828a5622ea8b50fe2463a3e7152dedd892
OmegaApproximasterEthereal2004 <sunnybunnies09@naver.com>
2025-11-16 22:08:45 +0900

    Implement Mojang API Breaking Changes
    the GameProfile is now "final" class, causing slimefun to disable and break.

--- dough-skins/src/main/java/io/github/bakedlibs/dough/skins/CustomGameProfile.java
+++ dough-skins/src/main/java/io/github/bakedlibs/dough/skins/CustomGameProfile.java
@@ -8,6 +8,8 @@ import javax.annotation.Nullable;
 
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
@@ -15,62 +17,51 @@ import com.mojang.authlib.properties.Property;
 import io.github.bakedlibs.dough.reflection.ReflectionUtils;
 import io.github.bakedlibs.dough.versions.MinecraftVersion;
 import io.github.bakedlibs.dough.versions.UnknownServerVersionException;
-import org.bukkit.profile.PlayerProfile;
-import org.bukkit.profile.PlayerTextures;
 
-public final class CustomGameProfile extends GameProfile {
+public final class CustomGameProfile {
 
-    /**
-     * The player name for this profile.
-     * "CS-CoreLib" for historical reasons and backwards compatibility.
-     */
     private static final String PLAYER_NAME = "CS-CoreLib";
-
-    /**
-     * The skin's property key.
-     */
     private static final String PROPERTY_KEY = "textures";
 
     private final URL skinUrl;
     private final String texture;
+    private final GameProfile delegate;
 
-    CustomGameProfile(@Nonnull UUID uuid, @Nullable String texture, @Nonnull URL url) {
-        super(uuid, PLAYER_NAME);
+    public CustomGameProfile(@Nonnull UUID uuid, @Nullable String texture, @Nonnull URL url) {
+        this.delegate = new GameProfile(uuid, PLAYER_NAME);
         this.skinUrl = url;
         this.texture = texture;
 
         if (texture != null) {
-            getProperties().put(PROPERTY_KEY, new Property(PROPERTY_KEY, texture));
+            this.delegate.getProperties().put(PROPERTY_KEY, new Property(PROPERTY_KEY, texture));
         }
     }
 
-    void apply(@Nonnull SkullMeta meta) throws NoSuchFieldException, IllegalAccessException, UnknownServerVersionException {
-        // setOwnerProfile was added in 1.18, but getOwningPlayer throws a NullPointerException since 1.20.2
+    public void apply(@Nonnull SkullMeta meta)
+            throws NoSuchFieldException, IllegalAccessException, UnknownServerVersionException {
         if (MinecraftVersion.get().isAtLeast(MinecraftVersion.parse("1.20"))) {
-            PlayerProfile playerProfile = Bukkit.createPlayerProfile(this.getId(), PLAYER_NAME);
+            PlayerProfile playerProfile = Bukkit.createPlayerProfile(this.delegate.getId(), PLAYER_NAME);
             PlayerTextures playerTextures = playerProfile.getTextures();
             playerTextures.setSkin(this.skinUrl);
             playerProfile.setTextures(playerTextures);
             meta.setOwnerProfile(playerProfile);
         } else {
-            // Forces SkullMeta to properly deserialize and serialize the profile
-            ReflectionUtils.setFieldValue(meta, "profile", this);
+            // Force SkullMeta to use our wrapped GameProfile
+            ReflectionUtils.setFieldValue(meta, "profile", this.delegate);
 
             meta.setOwningPlayer(meta.getOwningPlayer());
 
-            // Now override the texture again
-            ReflectionUtils.setFieldValue(meta, "profile", this);
+            // Override the texture again
+            ReflectionUtils.setFieldValue(meta, "profile", this.delegate);
         }
-
     }
 
-    /**
-     * Get the base64 encoded texture from the underline GameProfile.
-     *
-     * @return the base64 encoded texture.
-     */
     @Nullable
     public String getBase64Texture() {
         return this.texture;
+    }
+
+    public GameProfile getHandle() {
+        return this.delegate;
     }
 }
